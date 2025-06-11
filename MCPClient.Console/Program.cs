90a03274658501002c4be86c8a306cc16c7a7778
Fujio Kojima <fujiwo@shos.info>
2025-06-11 16:17:44 +0900

    プロジェクト ファイルを追加します。

---
+++ MCPClient.Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using ModelContextProtocol.Client;
+
+const string endPoint = "https://localhost:7052/sse";
+var clientTransport = new SseClientTransport(new SseClientTransportOptions { Endpoint = new Uri(endPoint) });
+IMcpClient client = await McpClientFactory.CreateAsync(clientTransport)
+                                          .ConfigureAwait(false);
+
+await Run(client, new Dictionary<string, object?> { ["location"] = "東京" });
+
+Console.ReadKey();
+
+/*
+client = await McpClientFactory.CreateAsync(
+    new StdioClientTransport(new() {
+        Command = "dotnet",
+        Arguments = ["run", "--project", @"C:\DropBox\Dropbox\202505.Microsoft BUILD\Source\MCPSample\MCPServer.Console\MCPServer.Console.csproj"],
+        Name = "TimeTools",
+    }));
+
+await Run(client);
+
+Console.ReadKey();
+ */
+
+async Task Run(IMcpClient client, IReadOnlyDictionary<string, object?>? arguments = null) {
+    // ツールの一覧からツールを取得
+    foreach (var tool in await client.ListToolsAsync()) {
+        Console.WriteLine($"{tool.Name} ({tool.Description})");
+
+        // ツールを実行
+        var response = await client.CallToolAsync(tool.Name, arguments);
+
+        // レスポンスを表示
+        foreach (var content in response.Content)
+            Console.WriteLine($"tool.Name: {tool.Name}, content.Type: {content.Type}, content.Text: {content.Text}");
+    }
+}
---
+++ MCPServer.Console/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+var builder = Host.CreateEmptyApplicationBuilder(settings: null);
+
+builder.Services
+       .AddMcpServer()
+       .WithStdioServerTransport()
+       .WithToolsFromAssembly();
+
+await builder.Build().RunAsync();
+
+[McpServerToolType]
+public static class TimeTools
+{
+    [McpServerTool, Description("現在の時刻を取得")]
+    public static string GetCurrentTime() => DateTimeOffset.Now.ToString();
+
+    [McpServerTool, Description("指定されたタイムゾーンの現在の時刻を取得")]
+    public static string GetTimeInTimezone(string timezone)
+    {
+        try {
+            var timeZone = TimeZoneInfo.FindSystemTimeZoneById(timezone);
+            return TimeZoneInfo.ConvertTime(DateTimeOffset.Now, timeZone).ToString();
+        } catch {
+            return "無効なタイムゾーンが指定されています";
+        }
+    }
+}
+
+/*
+npx @modelcontextprotocol/inspector dotnet run --project ./MCPServer.Console/MCPServer.Console.csproj
+http://127.0.0.1:6274
+
+
+Visual Studio Code - 設定 - MCP - settings.json で編集
+
+"servers": {
+    "MCPServer.Console": {
+        "type": "stdio",
+        "command": "dotnet",
+        "args": [
+            "run",
+            "--project",
+            "C:\\DropBox\\Dropbox\\202505.Microsoft BUILD\\Source\\MCPSample\\MCPServer.Console\\MCPServer.Console.csproj"
+        ]
+    }
+}
+ */
